4725cffa6722d653d8be9d60c7d3da934181fba5
Mylena Leite Bortolozzo <mylenaleiteb@gmail.com>
2020-09-01 17:45:57 -0300

    condicionais, laços de repetição e date

--- js/main.js
+++ js/main.js
@@ -17,10 +17,43 @@ console.log(lista.toString()[0]);
 console.log(lista.join(" - "));
 */
 
-//dicionário
+/*dicionário
 var fruta = {nome:"Maçã", cor:"Vermelha"};
 console.log(fruta.nome);
 
 //lista de dicionários
 var frutas = [{nome:"Maça", cor:"Vermelha"},{nome:"Uva", cor:"Roxa"}];
 console.log(frutas[1]); //elemento 1 que é o {nome:"Uva", cor:"Roxa"} e o nome nele é "Uva"
+*/
+
+/*
+var idade = prompt("Qual sua idade?");
+if(idade >= 18){
+    alert("Maior de idade");
+    }
+    else{
+        alert("Menor de idade");
+    };
+*/
+
+/*
+var count = 0
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count = count + 1 //não coloca "count = count++" porque vai travar tudo kkkkkk"
+    ou
+    count++ ;
+}]
+*/
+
+/*var count; // deu errado REVER
+for(count=0; count <= 5; count=++){
+    alert(count);
+};
+*/
+
+var d = new Date();
+alert(d.getDay());
+alert(d.getMonth());
+alert(d.getFullYear());
