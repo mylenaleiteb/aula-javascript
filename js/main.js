56331a69f98cec99c7aca968c039169a879a311e
Mylena Leite Bortolozzo <mylenaleiteb@gmail.com>
2020-09-01 16:19:17 -0300

    array e dicionarios

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 var nome="Mylena"
 var frase="Brasil é o melhor país do mundo"
 
@@ -5,3 +6,21 @@ console.log(nome);
 console.log(frase.replace("Brasil","Japão").toUpperCase());
 
 alert("Bem vinda " + nome);
+*/
+
+/*
+var lista= ["maça","pera","laranja"];
+lista.push("uva");
+//lista.pop()
+console.log(lista[0]);
+console.log(lista.toString()[0]);
+console.log(lista.join(" - "));
+*/
+
+//dicionário
+var fruta = {nome:"Maçã", cor:"Vermelha"};
+console.log(fruta.nome);
+
+//lista de dicionários
+var frutas = [{nome:"Maça", cor:"Vermelha"},{nome:"Uva", cor:"Roxa"}];
+console.log(frutas[1]); //elemento 1 que é o {nome:"Uva", cor:"Roxa"} e o nome nele é "Uva"
