2d84e963ad7ebc7a4897b41aeadfe6d34ad8ca4b
Mylena Leite Bortolozzo <mylenaleiteb@gmail.com>
2020-09-01 23:52:17 -0300

    desenvolvendo paginas web com javascript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,64 @@
+// DESENVOLVENDO PAGS WEB COM JAVASCRIPT
+
+function clica(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    //console.log(document.getElementById("agradecimento"));
+}
+
+function redirecionar(){
+    window.open("https://github.com/mylenaleiteb"); //abre em aba diferente
+    //ou
+    //window.location.href = "https://github.com/mylenaleiteb"; //abre na mesma aba
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("página carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+/*function soma(n1,n2){
+    return n1 + n2;
+}
+
+function validaIdade(idade){
+    var validar;
+    if(idade >=18 ){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual é sua idade? ")
+console.log(validaIdade(idade));
+alert(validaIdade(idade));
+
+//alert(soma(5,10));
+*/
+
+
+
+
+
+
+
+//INTRODUÇÃO AO JAVASCRIPT
+
 /*
 var nome="Mylena"
 var frase="Brasil é o melhor país do mundo"
@@ -44,7 +105,7 @@ while (count <= 5){
     count = count + 1 //não coloca "count = count++" porque vai travar tudo kkkkkk"
     ou
     count++ ;
-}]
+}
 */
 
 /*var count; // deu errado REVER
@@ -53,7 +114,10 @@ for(count=0; count <= 5; count=++){
 };
 */
 
+/*
 var d = new Date();
 alert(d.getDay());
 alert(d.getMonth());
 alert(d.getFullYear());
+*/
+
